65edb6341815652081767bbae333129963f1cfa0
Zachary Weidenbach <zweidenbach@gmail.com>
2024-05-31 11:16:23 -0700

    try adding base to build command

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -13,28 +13,31 @@ import { TodoRedux } from './routes/TodoRedux';
 import { TodoReactContext } from './routes/TodoReactContext';
 import { TodoZustand } from './routes/TodoZustand';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Navigate to="/todo/reactContext" replace />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/todo/reactContext',
-    element: <TodoReactContext />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/todo/redux',
-    element: <TodoRedux />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/todo/zustand',
-    element: <TodoZustand />,
-    errorElement: <ErrorPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Navigate to="/todo/reactContext" replace />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/todo/reactContext',
+      element: <TodoReactContext />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/todo/redux',
+      element: <TodoRedux />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/todo/zustand',
+      element: <TodoZustand />,
+      errorElement: <ErrorPage />,
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
